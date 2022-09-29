9b4f2ec668edc059d7c8a3ad0c92dd7979b1358e
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2022-09-29 13:42:53 +0100

    Saving working changes.

--- Assets/Scripts/scoreSystem.cs
+++ Assets/Scripts/scoreSystem.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class scoreSystem : MonoBehaviour
 {
+    float score = 0;
+    Rigidbody2D myRB;
+    Timer timerSystem;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();    
+        timerSystem = GetComponent<Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!timerSystem.GetTimesUp())
+        {
+            AddScore();
+        }
+    }
+
+    private void AddScore()
+    {
+
+        if (myRB.velocity.x > 0)
+        {
+            score += myRB.velocity.x;
+        }
+        Debug.Log(score);
     }
 }
