cfa275139e03355cb3d2965c2a4b635cdf225ae3
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2022-10-11 15:31:39 +0100

    To revert.

--- Assets/Scripts/wilboMovement.cs
+++ Assets/Scripts/wilboMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class wilboMovement : MonoBehaviour
 {
@@ -92,5 +93,18 @@ public class wilboMovement : MonoBehaviour
                 _timer.TimesUp();
             }
         }
+    }
+
+    void OnRestart()
+    {
+        Debug.Log("This should restart the game.");
+        SceneManager.LoadScene(0);
+
+    }
+
+    void OnQuit()
+    {
+        Debug.Log("This should quit the game.");
+        Application.Quit();
     }
 }
